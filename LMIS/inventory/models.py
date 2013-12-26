d23080d86325d7188eca1488ebb3bd24e7bba3f4
= <=>
2013-12-26 10:50:49 +0100

    added inventory and inventory line models

--- LMIS/inventory/models.py
+++ LMIS/inventory/models.py
@@ -1,3 +1,37 @@
+#import core django modules
 from django.db import models
 
-# Create your models here.
+#import project modules
+from core.models import Warehouse, BaseModel, Item, UnitOfMeasurement
+
+
+class Inventory(BaseModel):
+    """
+        This is used to track the current quantity of each product at a facility. each facility warehouse
+        (storage location) has an inventory.
+    """
+    warehouse = models.ForeignKey(Warehouse, blank=True, null=True)
+
+
+class InventoryLine(BaseModel):
+    """
+        This represents a single entry for a unique stocks in an inventory
+
+        The quantity unit of measurement is calculated from Item->product->base_uom, likewise product code
+
+        active - is used to indicate if the inventory line should be considered when calculating current quantity of
+        a product at a facility etc.
+    """
+    item = models.ForeignKey(Item)
+    inventory = models.ForeignKey(Inventory)
+    quantity = models.IntegerField()
+    weight = models.FloatField(blank=True, null=True)
+    weight_uom = models.ForeignKey(UnitOfMeasurement, blank=True, null=True)
+    volume = models.FloatField(blank=True, null=True)
+    volume_uom = models.ForeignKey(UnitOfMeasurement, blank=True, null=True)
+    active = models.BooleanField()
+
+
+
+
+
