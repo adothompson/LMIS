e3cb14f387feb5c660e2eab404ed6a41c81544f8
= <=>
2013-12-28 14:18:13 +0100

    updated CCE status

--- LMIS/cce/models.py
+++ LMIS/cce/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 
 #import external modules
-from django_extensions.db.fields import UUIDField
+from model_utils import Choices
 
 #import project app modules
 from core.models import BaseModel, UnitOfMeasurement, Facility, Warehouse
@@ -13,7 +13,6 @@ class ColdChainEquipmentType(BaseModel):
         This class is used to classify different types of ColdChainEquipment such as Freezers and Fridge etc
         Fridge has minimum of +2 degree celsius to +8 degree celsius.
     """
-    uuid = UUIDField(version=4, primary_key=True)
     code = models.CharField(max_length=20, unique=True)
     name = models.CharField(max_length=20, unique=True)
     minimum_temperature = models.FloatField(blank=True, null=True, verbose_name='min temp.')
@@ -29,15 +28,13 @@ class ColdChainEquipment(BaseModel):
         Cold Chain Equipment is used to store vaccines in order to avoid cold chain breach, could be freezer, fridge,
         insulated coolers etc.
     """
-    uuid = UUIDField(version=4, primary_key=True)
-    #TODO: model status with django model util
-    CCE_STATUS = ((0, "Good Working Condition"), (1, "Faulty"), (2, "Not Working"))
+    STATUS = Choices('Green', 'Yellow', 'Red')
     code = models.CharField(max_length=35, unique=True)
     gross_capacity = models.FloatField(blank=True, null=True)
     net_capacity = models.FloatField(blank=True, null=True)
     capacity_uom = models.ForeignKey(UnitOfMeasurement, null=True, blank=True)
-    cce_type = models.ForeignKey(ColdChainEquipmentType)
-    cce_status = models.IntegerField(default=0, choices=CCE_STATUS)
+    type = models.ForeignKey(ColdChainEquipmentType)
+    status = models.CharField(max_length=20, choices=STATUS)
     facility = models.ForeignKey(Facility)
     storage_location = models.ForeignKey(Warehouse, blank=True, null=True)
 
@@ -49,21 +46,19 @@ class CCETemperatureLog(BaseModel):
     """
         Used to keep log or CCE temperature readings
     """
-    uuid = UUIDField(version=4, primary_key=True)
     temperature = models.FloatField()
     temperature_uom = models.ForeignKey(UnitOfMeasurement)
     cce = models.ForeignKey(ColdChainEquipment)
     date_time_logged = models.DateTimeField()
 
     def __str__(self):
-        return '{temp} {uom_symbol}'.format(temp=self.temperature, uom_symbol=self.temperature_uom.symbol)
+        return '{temp}'.format(temp=self.temperature)
 
 
 class CCEProblemLog(BaseModel):
     """
         This models the problem log for CCEs
     """
-    uuid = UUIDField(version=4, primary_key=True)
     name = models.CharField(max_length=35)
     cce = models.ForeignKey(ColdChainEquipment)
     description = models.CharField(max_length=100, blank=True)
