cb14c97ba994889fac5eed69a6f24a3f928fafc3
= <=>
2014-01-04 16:08:12 +0100

    got api/products working but cant do migrate on locations app hence i commented it out.

--- LMIS/core/urls.py
+++ LMIS/core/urls.py
@@ -8,6 +8,6 @@ router.register(r'api/products', views.ProductViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browseable API.
 urlpatterns = patterns('',
-    url(r'^', include(router.urls)),
+    url(r'^core/', include(router.urls)),
     #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
