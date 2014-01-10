4c17014980dcdb3901181c6322c1253f2b6d4050
= <=>
2014-01-10 09:55:02 +0100

    created view-set for cce REST API

--- LMIS/cce/views.py
+++ LMIS/cce/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+"""
+    cce/views.py holds the API end-point for CCE app. it is used to define the API views from which external
+    clients can interact with the CCE app to perform operations such as Create, Retrieve, Update, Delete etc on the CCE
+    models via ModelViewSet functions such as list(), retrieve(), update(), partial_update(), destroy() and ad-hoc
+    functions can be added too.
+"""
 
-# Create your views here.
+#import project modules
+from core.views import BaseModelViewSet
+from .models import ColdChainEquipment
+from .api.serializers import CCESerializer
+
+
+class CCEViewSet(BaseModelViewSet):
+    """
+        REST API end-point for CCE that allows CRUD operations etc to be performed on CCE models via REST url
+    """
+    queryset = ColdChainEquipment.objects.all()
+    serializer_class = CCESerializer
