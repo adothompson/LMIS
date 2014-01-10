4eb412053fb2a41d7e1c8e3f9257cbc96c532af6
= <=>
2014-01-10 10:07:03 +0100

    added model serializer for ColdChainEquipmentType

--- LMIS/cce/api/serializers.py
+++ LMIS/cce/api/serializers.py
@@ -7,12 +7,20 @@ from rest_framework import serializers
 
 #import project modules
 from core.api.serializers import BaseModelSerializer
-from cce.models import ColdChainEquipment
+from cce.models import ColdChainEquipment, ColdChainEquipmentType
 
 
 class CCESerializer(BaseModelSerializer):
     """
-        This is serializer for CCE model
+        This is serializer for ColdChainEquipment model
     """
     class Meta:
         model = ColdChainEquipment
+
+
+class CCETypeSerializer(BaseModelSerializer):
+    """
+        This is serializer for ColdChainEquipmentType model
+    """
+    class Meta:
+        model = ColdChainEquipmentType
