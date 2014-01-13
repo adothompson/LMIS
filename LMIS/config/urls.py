8eaabde2c1118a40fa1553ac8de6e7428fa43334
Taylor Baldwin <taylor.baldwin@ehealthnigeria.org>
2014-01-13 14:08:21 +0100

    fixed merge conflict

--- LMIS/config/urls.py
+++ LMIS/config/urls.py
@@ -10,11 +10,8 @@ urlpatterns = patterns('',
                        url(r'api/v1/core/', include('core.api.urls')),
                        url(r'api/v1/cce/', include('cce.api.urls')),
                        url(r'api/v1/facilities/', include('facilities.api.urls')),
-<<<<<<< HEAD
+                       url(r'api/v1/inventory/', include('inventory.api.urls')),
 
                        # DRF browsable API urls
                        url(r'^api-web/', include('rest_framework.urls', namespace='rest_framework'))
 )
-=======
-                       url(r'api/v1/inventory/', include('inventory.api.urls')),)
->>>>>>> 6c3dbb4ce21b85ebf68be28887a4cf697b0e6c6f
